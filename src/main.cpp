b8cc43f217c543d969651b82291484ceb342408b
LumisDev <153009693+LumisDev@users.noreply.github.com>
2024-01-13 14:45:59 +0100

    Add bootstrapped project

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+#include <SFML/Graphics.hpp>
+#include <box2d/box2d.h>
 int main(){
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Blappers!");
+    b2::World world;
+    while (window.isOpen()){
+        sf::Event e;
+        while(window.pollEvent(e)){
+            if(e.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+        window.clear();
+        window.display()
+    }
     return 0;
 }
